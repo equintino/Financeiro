ce0f1b43b2f9b4f6960b4cb058f9cfab354d92d2
Casa <emquintino@ig.com.br>
2017-07-14 00:56:24 -0300

    calendario

--- web/js/jquery.js
+++ web/js/jquery.js
@@ -86,6 +86,29 @@ $(document).ready(function() {
                 var $w = $(w.document.body);
                 $w.html();
       })
+         $(':input[name=dia]').click(function(){
+            $(this).datepicker({
+               dateFormat: 'dd',
+               dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
+               dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+               dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+               monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+               monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
+           });           
+         })
+         $('#sexo').css({
+            'background': '#ccc'
+         });
+         $(':input[name^=dt]').click(function(){
+            $(this).datepicker({
+               dateFormat: 'dd/mm/yy',
+               dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
+               dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+               dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+               monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+               monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
+           });           
+         })
 });
 
 
